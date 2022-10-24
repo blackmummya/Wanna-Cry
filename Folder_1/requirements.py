10068c83c9a2707f2e98b8bdacad0e9c183cb71d
MUMMYA <108289898+blackmummya@users.noreply.github.com>
2022-10-25 02:21:50 +0300

    Add files via upload

---
+++ Folder_1/requirements.py
@@ -0,0 +1,13 @@
+from os import system
+
+
+def req():
+    system('pip install pyautogui')
+    system('cls')
+    system('pip install python-telegram-bot')
+    system('cls')
+    system('pip install pyinstaller')
+    system('cls')
+
+
+req()
